55c96e8e0ba1d1562467fe7f6906c2da0bd34527
MariaEvpalova <mashayevpalova@gmail.com>
2022-11-19 03:15:28 +0300

    Complete input with buttons

--- script.js
+++ script.js
@@ -13,40 +13,45 @@ function getComputerChoice() {
     }
 }
 
-function getRoundResult(playerSelection, computerSelection) {
-    let proccess = (str) => (str.toLowerCase().charAt(0).toUpperCase() + str.toLowerCase().slice(1));
-    playerSelection = proccess(playerSelection);
-    computerSelection = proccess(computerSelection);
-    if (playerSelection == computerSelection)
-        return (`No winnner! ${playerSelection} and ${computerSelection}`);
-    else if (playerSelection == "Rock" && computerSelection == "Paper" || 
-             playerSelection == "Paper" && computerSelection == "Scissors" ||
-             playerSelection == "Scissors" && computerSelection == "Rock")
-            return `You lose! ${computerSelection} beats ${playerSelection}`;
-    else return `You win! ${playerSelection} beats ${computerSelection}`;
+function proccessString(str) {
+    return str.toLowerCase().charAt(0).toUpperCase() + str.toLowerCase().slice(1);
 }
 
-function game() {
-    playerScore = 0;
-    computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        console.log(round());
+function playRound(playerSelection) {
+    playerSelection = proccessString(playerSelection);
+    let computerSelection = proccessString(getComputerChoice());
+    if (playerSelection == computerSelection)
+        console.log(`No winnner! ${playerSelection} and ${computerSelection}`);
+    else if (playerSelection == "Rock" && computerSelection == "Paper" || 
+            playerSelection == "Paper" && computerSelection == "Scissors" ||
+            playerSelection == "Scissors" && computerSelection == "Rock") {
+                computerScore++;
+                console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            }
+    else {
+        playerScore++;
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
     }
+    roundCount++;
+    if (roundCount >= 5) restartGame();
+}
+
+function restartGame() {
     if (playerScore > computerScore) console.log("You win!\nCongratulations!");
     else if (computerScore > playerScore) console.log("Computer wins!");
     else {
         while (playerScore == computerScore) console.log(round());
         console.log(playerScore > computerScore ? "You win!\nCongratulations!" : "Computer wins!");
     }
+    console.log("\nNew game");
+    playerScore = 0, computerScore = 0, roundCount = 0;
 }
 
-function round() {
-    let roundResult = getRoundResult(prompt("What's your choice? "), getComputerChoice());
-    roundResult.includes("You win!") ? playerScore++ : computerScore++;
-    return roundResult;
-}
+let playerScore = 0;
+let computerScore = 0;
+let roundCount = 0;
 
-let playerScore;
-let computerScore;
-
-game();
+const buttons = Array.from(document.querySelectorAll('button'));
+buttons.forEach((button) => {
+    button.addEventListener('click', () => playRound(button.innerText));
+});
