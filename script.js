3e427c1ca3844c679dc51f7bd7db94f5ba8cb95b
MariaEvpalova <mashayevpalova@gmail.com>
2022-11-22 17:40:14 +0300

    Add simple UI Minor changes in game logic and input

--- script.js
+++ script.js
@@ -20,41 +20,41 @@ function proccessString(str) {
 function playRound(playerSelection) {
     playerSelection = proccessString(playerSelection);
     let computerSelection = proccessString(getComputerChoice());
-    if (playerSelection == computerSelection)
-        resultDiv.innerText = `No winnner! ${playerSelection} and ${computerSelection}`;
+    if (playerSelection == computerSelection) {
+        playerResult.innerText = playerResult.innerText.slice(0, -1) + (++playerScore);
+        computerResult.innerText = computerResult.innerText.slice(0, -1) + (++computerScore);
+        winReport.innerText = `${playerSelection} and ${computerSelection}`;
+
+    }
     else if(playerSelection == "Rock" && computerSelection == "Paper" || 
             playerSelection == "Paper" && computerSelection == "Scissors" ||
             playerSelection == "Scissors" && computerSelection == "Rock") {
-                computerScore++;
-                resultDiv.innerText = `You lose! ${computerSelection} beats ${playerSelection}`;
+                computerResult.innerText = computerResult.innerText.slice(0, -1) + (++computerScore);
+                winReport.innerText = `Computer wins. ${computerSelection} beats ${playerSelection}`;
             }
     else {
-        playerScore++;
-        resultDiv.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
+        playerResult.innerText = playerResult.innerText.slice(0, -1) + (++playerScore);
+        winReport.innerText = `You win! ${playerSelection} beats ${computerSelection}`;
     }
-    roundCount++;
-    if (roundCount >= 5) restartGame();
+    if (playerScore >= 5 || computerScore >= 5) restartGame();
 }
 
 function restartGame() {
-    if (playerScore > computerScore) resultDiv.innerText = "You win!\nCongratulations!";
-    else if (computerScore > playerScore) resultDiv.innerText = "Computer wins!";
-    else {
-        while (playerScore == computerScore) resultDiv.innerText = round();
-        resultDiv.innerText = playerScore > computerScore ? "You win!\nCongratulations!" : "Computer wins!";
-    }
-    resultDiv.innerText += "\n\nPress a button to begin a new game";
-    //console.log("\nNew game");
-    playerScore = 0, computerScore = 0, roundCount = 0;
+    winReport.innerText = (playerScore > computerScore ? "Congratulations! You win the game :)" : "Computer beats you :(" 
+                           + "\nSelect your next choice to begin a new game");
+    playerScore = 0, computerScore = 0;
+    playerResult.innerText = playerResult.innerText.slice(0, -1) + playerScore;
+    computerResult.innerText = computerResult.innerText.slice(0, -1) + computerScore;
 }
 
 let playerScore = 0;
 let computerScore = 0;
-let roundCount = 0;
 
 const buttons = Array.from(document.querySelectorAll('button'));
 buttons.forEach((button) => {
     button.addEventListener('click', () => playRound(button.innerText));
 });
 
-const resultDiv = document.querySelector(".result");
+const winReport = document.querySelector(".win");
+const playerResult = document.querySelector(".score > .player");
+const computerResult = document.querySelector(".score > .computer");
